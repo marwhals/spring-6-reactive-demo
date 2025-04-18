4cd4d825e41a8ea61e6e8e30be72588e69cdded1
marwhals <marjan.mubarok2012@gmail.com>
2025-04-18 12:11:20 +0100

    Flux Operations

--- src/test/java/spring/example/reactive_demo/repositories/PersonRepositoryImplTest.java
+++ src/test/java/spring/example/reactive_demo/repositories/PersonRepositoryImplTest.java
@@ -1,8 +1,11 @@
 package spring.example.reactive_demo.repositories;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import spring.example.reactive_demo.domain.Person;
+
+import java.util.List;
 
 class PersonRepositoryImplTest {
 
@@ -31,14 +34,52 @@ class PersonRepositoryImplTest {
     void testMapOperation() {
         Mono<Person> personMono = personRepository.getById(1);
 
-        personMono.map(person -> { //😱😱😱😱 Back from the dead 🤣?
-            return person.getFirstName();
-        }).subscribe(firstName -> {
-            System.out.println(firstName);
-        });
+        //😱😱😱😱 Back from the dead 🤣?
+        personMono.map(Person::getFirstName).subscribe(System.out::println);
 
-
+        //Equivalent but more verbose
+//        personMono.map(person -> { //😱😱😱😱 Back from the dead 🤣?
+//            return person.getFirstName();
+//        }).subscribe(firstName -> {
+//            System.out.println(firstName);
+//        });
     }
 
+    @Test
+    void testFluxBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Person person = personFlux.blockFirst();
+
+        System.out.println(person.toString());
+    }
+
+    @Test
+    void testFluxSubscriber() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.subscribe(person -> {
+            System.out.println(person.toString());
+        });
+    }
+
+    @Test
+    void testFluxMap() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.map(Person::getFirstName).subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Mono<List<Person>> listMono = personFlux.collectList();
+
+        listMono.subscribe(list -> {
+            list.forEach(person -> System.out.println(person.toString()));
+        });
+
+    }
 
 }
